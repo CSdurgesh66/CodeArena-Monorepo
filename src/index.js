32c7cdf331e4044f31b5be52067acb8802c54936
Durgesh Kumar <durgesh42416@gmail.com>
2026-01-08 16:46:09 +0530

    Feat: Added basic express server setup with dotenv and nodemon

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const {PORT} = require('./config/server.config');
+
+const app = express();
+
+
+app.get('/ping',(req,res) => {
+    return res.json({message:'Problem Service is alive'});
+})
+
+app.listen(PORT,() => {
+    console.log(`Server started at Port: ${PORT}`);
+});
