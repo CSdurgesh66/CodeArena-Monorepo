7c0bcb05e4d873bda833699b44f6649082cd6cdc
Durgesh Kumar <durgesh42416@gmail.com>
2026-01-08 17:46:59 +0530

    Feat: Added routing structure for versioned api for problems

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const {PORT} = require('./config/server.config');
 
+const apiRouter = require('./routes/index')
+
 const app = express();
 
+
+app.use('/api',apiRouter);
 
 app.get('/ping',(req,res) => {
     return res.json({message:'Problem Service is alive'});
