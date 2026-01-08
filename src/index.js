c1d127c3a13618e92bdbc7d8825ef1e07df36da9
Durgesh Kumar <durgesh42416@gmail.com>
2026-01-08 23:43:08 +0530

    Feat: Added error handler middleware with basic error classes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const {PORT} = require('./config/server.config');
 
-const apiRouter = require('./routes/index')
+const apiRouter = require('./routes/index');
+const BaseError = require('./errors/base.error');
+const errorHandler = require('./utils/errorHandler');
 
 const app = express();
 
@@ -13,6 +15,11 @@ app.get('/ping',(req,res) => {
     return res.json({message:'Problem Service is alive'});
 })
 
+
+app.use(errorHandler);
+
 app.listen(PORT,() => {
     console.log(`Server started at Port: ${PORT}`);
+   
+    
 });
